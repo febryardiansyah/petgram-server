24efab8672c5b3b6ebb7ba06722f4734c412a782
febryardiansyah <febrymuhammad80@gmail.com>
2020-09-22 18:06:23 +0700

    hide .env

--- src/helpers/base-url.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    baseUrl : process.env.MODE === 'PRODUCTION'
-    ? 'https://petgram-server.herokuapp.com/': 'http://localhost:3000/',
-}
--- src/helpers/imageType.js
+++
@@ -1,6 +0,0 @@
-module.exports = (image,name) => {
-    const splitedName = image.name.split(".");
-    const imageType = splitedName[splitedName.length - 1];
-    const fileName = `${name}-${Date.now()}.${imageType}`;
-    return fileName;
-}
