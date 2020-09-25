11ca1da33545a1bddc3a323b30715620e77de6ef
febryardiansyah <febrymuhammad80@gmail.com>
2020-09-25 18:20:51 +0700

    test commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use("/image/post", express.static("./src/images/post"));
 app.use("/", (req, res) => {
   res.send({
     status: "I love you, but you love him 😢",
-    message: "Welcome to Petgram Server 😳👍",
+    message: "",
   });
 });
 app.use("*", (req, res) => {
